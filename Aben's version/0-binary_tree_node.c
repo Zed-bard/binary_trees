0f874eeb287b8dca9353c53dc010b05d47af3fd1
Abenezer Endalew <abenezeralebachew3@gmail.com>
2024-02-27 20:31:02 +0300

    Create node

---
+++ Aben's version/0-binary_tree_node.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node
+ * @value: value to put in the new node
+ * Returns: pointer to the new node or NULL
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = new_node->right = NULL;
+
+	if (parent != NULL)
+	{
+		if (parent->left == NULL)
+			parent->left = new_node;
+		else
+			parent->right = new_node;
+	}
+
+	return (new_node);
+}
+
+int main(void)
+{
+    binary_tree_t *root;
+
+    root = binary_tree_node(NULL, 98);
+
+    root->left = binary_tree_node(root, 12);
+    root->left->left = binary_tree_node(root->left, 6);
+    root->left->right = binary_tree_node(root->left, 16);
+
+    root->right = binary_tree_node(root, 402);
+    root->right->left = binary_tree_node(root->right, 256);
+    root->right->right = binary_tree_node(root->right, 512);
+
+    binary_tree_print(root);
+    return (0);
+}
